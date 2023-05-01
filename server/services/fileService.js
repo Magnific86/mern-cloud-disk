91f71fc90ac08862c1970b14977084854c1beee5
Magnific86 <112124588+Magnific86@users.noreply.github.com>
2023-05-01 12:18:33 +0300

    idk

--- server/services/fileService.js
+++ server/services/fileService.js
@@ -1,48 +1,23 @@
-// const fs = require("fs");
-// const File = require("../models/File.js");
-// const config = require("config");
-
-// class FileService {
-//   createDir(file) {
-//     const configFilePath = config.get("filePath");
-//     const filePath = `${configFilePath}\\${file.user}\\${file.path}`;
-//     return new Promise((resolve, reject) => {
-//       try {
-//         if (!fs.existsSync(filePath)) {
-//           fs.mkdirSync(filePath);
-//           return resolve({ message: "File was created" });
-//         } else {
-//           return reject({ message: "File path already exist" });
-//         }
-//       } catch (e) {
-//         return reject({ message: "File error" });
-//       }
-//     });
-//   }
-// }
-
-// module.exports = new FileService();
-
-const fs = require("fs");
-const File = require("../models/File");
-const config = require("config");
+const fs = require("fs")
+const File = require("../models/File")
+const config = require("config")
 
 class FileService {
-  createDir(file) {
-    const filePath = `${config.get("filePath")}\\${file.user}\\${file.path}`;
-    return new Promise((resolve, reject) => {
-      try {
-        if (!fs.existsSync(filePath)) {
-          fs.mkdirSync(filePath);
-          return resolve({ message: "File was created" });
-        } else {
-          return reject({ message: "File already exist" });
-        }
-      } catch (e) {
-        return reject({ message: "File error" });
-      }
-    });
-  }
+    createDir(file) {
+        const filePath = `${config.get("filePath")}\\${file.user}\\${file.path}`
+        return new Promise((resolve, reject) => {
+            try {
+                if (!fs.existsSync(filePath)) {
+                    fs.mkdirSync(filePath)
+                    return resolve({ message: "File was created" })
+                } else {
+                    return reject({ message: "File already exist" })
+                }
+            } catch (e) {
+                return reject({ message: "File error" })
+            }
+        })
+    }
 }
 
-module.exports = new FileService();
+module.exports = new FileService()
